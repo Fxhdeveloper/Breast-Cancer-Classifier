29b62101bf1e570d396a673b3e7010e54c351aa2
Fxhdeveloper <festimxhoxhaj@gmail.com>
2019-02-07 00:54:38 +0100

    Find the highest Accuracy function

--- main_script.py
+++ main_script.py
@@ -3,6 +3,16 @@ from sklearn.model_selection  import train_test_split
 from sklearn.neighbors        import KNeighborsClassifier
 from matplotlib               import pyplot as plt 
 
+#Find the highest Accuracy of the classifier and the k for which it was reached 
+def find_highest_accuracy(accuracies, k_list):
+    highest_accuracy = 0
+    k=0
+    for accuracy in accuracies:
+        if highest_accuracy < accuracy:
+        highest_accuracy = accuracy
+        k =accuracies.index(accuracy)
+    return highest_accuracy, k  
+
 #load dataset
 breast_cancer_data = load_breast_cancer()
 #split train and validation data
@@ -15,7 +25,10 @@ for k in range(1,101):
   classifier = KNeighborsClassifier(n_neighbors = k)
   classifier.fit(training_data, training_labels)
   accuracies.append(classifier.score(validation_data,validation_labels ))
+
 k_list = range(1,101)
+Accuracy, k = find_highest_accuracy(accuracies, k_list)
+print("Highest accuracy is: {:.3f} which is reached first with k = {} ".format(Accuracy, k))
 
 #plot accuracies for different k coefficients
 plt.plot(k_list, accuracies)
